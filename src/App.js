edc61af0a0a335a5891023583a52ca40084ccec9
Kevin Meixner <kmeixner42@gmail.com>
2023-06-09 10:14:16 -0400

    Added some basic layout and display logic
    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+
+const MONDAY = 0;
+const TUESDAY = 1;
+const WEDNESDAY = 2;
+const THURSDAY = 3;
+const FRIDAY = 4;
+const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+const COOKS = 0;
+const WAITERS = 1;
+const staffType = ['Cooks', 'Waiters'];
+
+class StaffDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stafftype: 0,
+	  weekday: 0,
+    }	
+  }
+  render() {
+    return (
+    <div>
+		<h1>{daysOfWeek[this.state.stafftype]}</h1>
+		
+		<h2>{daysOfWeek[this.state.weekday]}</h2>
+		
+		<ul>
+			<li>No Staff Scheduled</li>
+		</ul>
+		
+		<button disabled={this.state.weekday < TUESDAY}>Prev</button>
+		<button disabled={this.state.weekday > THURSDAY}>Next</button>
+    </div>
+    );
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<StaffDisplay />
     </div>
   );
 }
